76a1070a113e6537ce1582345bed1e08612d90a2
koki1124 <38421061+koki1124@users.noreply.github.com>
2018-11-19 16:48:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -15,7 +15,7 @@ var separate_time =function(time){
 }
 
 var now = new Date();
-var target = new Date(1811,24,24,0,0,0,0);//日時指定
+var target = new Date(2020,24,24,0,0,0,0);//日時指定
 var diff = target.getTime() - now.getTime();
 var counter = separate_time(diff);
 document.getElementById('countdown').textContent =
