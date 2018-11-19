55432a8203c42c9077cf673c256897bc5fb9961f
koki1124 <38421061+koki1124@users.noreply.github.com>
2018-11-19 16:03:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,27 @@
+//繰り返し
 for(var i=1;i<10;i++){
 var li =document.createElement('li');
 li.textContent=i+'回';
 document.getElementById('list').appendChild(li);
 }
+
+//カウント
+var separate_time =function(time){
+  var sec =time.getSeconds();
+  var min =time.getMinutes();
+  var hours =time.getHours();
+  var days =time.getDate();
+  var month =time.getMonth();
+  var year =time.getFullYrar();
+  return [sec, min, hours, days, month, year];
+}
+
+var now = new date();
+var counter = separate_time(now);
+document.getElementById('countdown').textContent =
+  counter[5]+'年'+
+  counter[4]+'月'+
+  counter[3]+'日'+
+  counter[2]+'時'+
+  counter[1]+'分'+
+  counter[0]+'秒';
