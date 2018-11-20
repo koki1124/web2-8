8739339c88811df2eb35a9c1162e09ea23383988
koki1124 <38421061+koki1124@users.noreply.github.com>
2018-11-20 11:08:00 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -72,10 +72,10 @@ function getCookie(c_name){
   var st="";
   var ed="";
   if(0 < document.cookie.length){
-    st=document.cookie.index0f(c_name + "=");
+    st=document.cookie.indexOf(c_name + "=");
     if(st!=-1){
       st=st+c_name.length+1;
-      ed=document.cookie.index0f(";",st);
+      ed=document.cookie.indexOf(";",st);
       if(ed==-1) ed=document.cookie.length;
       return unescape(document.cookie.substring(st,ed));
     }
