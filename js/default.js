95339638f29863def667abcf22f6bb4a4b3b0325
koki1124 <38421061+koki1124@users.noreply.github.com>
2018-11-20 09:54:51 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,35 +1,4 @@
-//繰り返し
-for(var i=1;i<11;i++){
-var li =document.createElement('li');
-li.textContent=i+'回';
-document.getElementById('list').appendChild(li);
-}
 
-//カウント
-var separate_time =function(time){
-  var sec =Math.floor((time / 1000) % 60);
-  var min =Math.floor((time / 1000 / 60) % 60);
-  var hours =Math.floor((time / 1000 / 60 / 60) % 24);
-  var days =Math.floor(time / 1000 / 60 / 60 / 24);
-  return [sec, min, hours, days];
-}
-var update = function(){
-var now = new Date();
-var target = new Date(2020,24,24,0,0,0,0);//日時指定
-var diff = target.getTime() - now.getTime();
-var counter = separate_time(diff);
-document.getElementById('countdown').textContent =
-  '世界滅亡まで'+
-  counter[3]+'日'+
-  counter[2]+'時'+
-  counter[1]+'分'+
-  counter[0]+'秒';
-refresh();//タイマー
-}
-var refresh = function(){
-  setTimeout(update, 1000);
-}
-update();
 
 //画面移動
 function getFileName(){
