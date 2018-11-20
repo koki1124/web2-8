8dd944a2e9756e4a00458ffb1be66dc67ab77724
koki1124 <38421061+koki1124@users.noreply.github.com>
2018-11-20 10:21:41 +0900

    Update default.js

--- js2/default.js
+++ js2/default.js
@@ -1,1 +1,18 @@
+//画面移動
+function getFileName(){
+  return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value="other.html"]');
+}else{
+  opt = document.querySelector('option[value="index.html"]');
+}
+opt.select = true;
+
+document.getElementById('form').select.onchange = function(){
+  location.href = document.getElementById('form').select.value;
+}
 
