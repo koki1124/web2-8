10f44bb298fc76c28bd1fd4a894aae0c78347a14
koki1124 <38421061+koki1124@users.noreply.github.com>
2018-11-20 11:20:06 +0900

    Update default.js

--- js2/default.js
+++ js2/default.js
@@ -60,4 +60,6 @@ if(last_date){
 var current_time = new Date();
 setCookie('lastDate', current_time.toString(), 7);
 
-
+document.getElementById('remove_cookie').onsubmit = function(){
+  setCookie('lastDate',"", 0);
+};
